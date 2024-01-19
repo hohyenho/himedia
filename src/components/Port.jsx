f65d56a6c83f8b90d9e717bd65f96aca8ba82589
hohyenho <75921113+hohyenho@users.noreply.github.com>
2024-01-19 12:33:12 +0900

    2_이미지 추가

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+import port01 from "../assets/img/port01.jpg";
+import port02 from "../assets/img/port02.jpg";
+import port03 from "../assets/img/port03.jpg";
+import port04 from "../assets/img/port01.jpg";
+import port05 from "../assets/img/port02.jpg";
+import port06 from "../assets/img/port03.jpg";
+
 
 const Port = () => {
 	return (
@@ -11,7 +18,7 @@ const Port = () => {
 					<article class="port__item p1">
 						<span class="num">01.</span>
 						<a href="http://jok874.dothome.co.kr/main/" target="_blank" class="img">
-							<img src="img/port01.jpg" alt="TeamACE 포트폴리오" />
+							<img src={port01} alt="TeamACE 포트폴리오" />
 						</a>
 						<h3 class="title">Team Ace 팀프로젝트</h3>
 						<p class="desc">
@@ -24,7 +31,7 @@ const Port = () => {
 					<article class="port__item p2">
 						<span class="num">02.</span>
 						<a href="http://kimmina.dothome.co.kr/hwasung/" class="img" target="_blank">
-							<img src="img/port02.jpg" alt="화성 포트폴리오" />
+							<img src={port02} alt="화성 포트폴리오" />
 						</a>
 						<h3 class="title">화성 포트폴리오</h3>
 						<p class="desc">
@@ -37,7 +44,7 @@ const Port = () => {
 					<article class="port__item p3">
 						<span class="num">03.</span>
 						<a href="http://dongbinhosting.dothome.co.kr/team/" class="img" target="_blank">
-							<img src="img/port03.jpg" alt="C팀 포트폴리오" />
+							<img src={port03} alt="C팀 포트폴리오" />
 						</a>
 						<h3 class="title">Team_C 포트폴리오</h3>
 						<p class="desc">
@@ -50,7 +57,7 @@ const Port = () => {
 					<article class="port__item p4">
 						<span class="num">04.</span>
 						<a href="http://jok874.dothome.co.kr/main/" target="_blank" class="img">
-							<img src="img/port01.jpg" alt="TeamACE 포트폴리오" />
+							<img src={port04} alt="TeamACE 포트폴리오" />
 						</a>
 						<h3 class="title">Team Ace 팀프로젝트</h3>
 						<p class="desc">
@@ -63,7 +70,7 @@ const Port = () => {
 					<article class="port__item p5">
 						<span class="num">05.</span>
 						<a href="http://kimmina.dothome.co.kr/hwasung/" class="img" target="_blank">
-							<img src="img/port02.jpg" alt="화성 포트폴리오" />
+							<img src={port05} alt="화성 포트폴리오" />
 						</a>
 						<h3 class="title">화성 포트폴리오</h3>
 						<p class="desc">
@@ -76,7 +83,7 @@ const Port = () => {
 					<article class="port__item p6">
 						<span class="num">06.</span>
 						<a href="http://dongbinhosting.dothome.co.kr/team/" class="img" target="_blank">
-							<img src="img/port03.jpg" alt="여다슬 포트폴리오" />
+							<img src={port06} alt="여다슬 포트폴리오" />
 						</a>
 						<h3 class="title">Team_C 포트폴리오</h3>
 						<p class="desc">
