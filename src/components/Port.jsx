a72d59476ec3216d6a7516592336ae4e676a09d5
hohyenho <75921113+hohyenho@users.noreply.github.com>
2024-01-19 12:51:50 +0900

    3_portTExt 추가

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -6,6 +6,56 @@ import port04 from "../assets/img/port01.jpg";
 import port05 from "../assets/img/port02.jpg";
 import port06 from "../assets/img/port03.jpg";
 
+const portTExt = [
+	{
+		num: "01",
+		site: "http://jok874.dothome.co.kr/main/",
+		img: port01,
+		title: "Team Ace 팀프로젝트",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam tenetur in dolores officia voluptatum?",
+
+	},
+	{
+		num: "02",
+		site: "http://kimmina.dothome.co.kr/hwasung/",
+		img: port02,
+		title: "화성 포트폴리오",
+		desc: "2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam tenetur in dolores officia voluptatum?",
+
+	},
+	{
+		num: "03",
+		site: "http://dongbinhosting.dothome.co.kr/team/",
+		img: port03,
+		title: "C팀 포트폴리오",
+		desc: "3Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam tenetur in dolores officia voluptatum?",
+
+	},
+	{
+		num: "04",
+		site: "http://jok874.dothome.co.kr/main/",
+		img: port04,
+		title: "Team Ace 팀프로젝트",
+		desc: "4Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam tenetur in dolores officia voluptatum?",
+
+	},
+	{
+		num: "05",
+		site: "http://kimmina.dothome.co.kr/hwasung/",
+		img: port05,
+		title: "화성 포트폴리오",
+		desc: "5Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam tenetur in dolores officia voluptatum?",
+
+	},
+	{
+		num: "06",
+		site: "http://dongbinhosting.dothome.co.kr/team/",
+		img: port06,
+		title: "C팀 포트폴리오",
+		desc: "6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam tenetur in dolores officia voluptatum?",
+
+	},
+]
 
 const Port = () => {
 	return (
@@ -15,84 +65,19 @@ const Port = () => {
 					portfolio <em>포폴 작업물</em>
 				</div>
 				<div class="port__wrap">
-					<article class="port__item p1">
-						<span class="num">01.</span>
-						<a href="http://jok874.dothome.co.kr/main/" target="_blank" class="img">
-							<img src={port01} alt="TeamACE 포트폴리오" />
-						</a>
-						<h3 class="title">Team Ace 팀프로젝트</h3>
-						<p class="desc">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam
-							facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam
-							tenetur in dolores officia voluptatum?
-						</p>
-						<a href="http://jok874.dothome.co.kr/main/" target="_blank" class="site">사이트 보기</a>
-					</article>
-					<article class="port__item p2">
-						<span class="num">02.</span>
-						<a href="http://kimmina.dothome.co.kr/hwasung/" class="img" target="_blank">
-							<img src={port02} alt="화성 포트폴리오" />
-						</a>
-						<h3 class="title">화성 포트폴리오</h3>
-						<p class="desc">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam
-							facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam
-							tenetur in dolores officia voluptatum?
-						</p>
-						<a href="http://kimmina.dothome.co.kr/hwasung/" target="_blank" class="site">사이트 보기</a>
-					</article>
-					<article class="port__item p3">
-						<span class="num">03.</span>
-						<a href="http://dongbinhosting.dothome.co.kr/team/" class="img" target="_blank">
-							<img src={port03} alt="C팀 포트폴리오" />
-						</a>
-						<h3 class="title">Team_C 포트폴리오</h3>
-						<p class="desc">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam
-							facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam
-							tenetur in dolores officia voluptatum?
-						</p>
-						<a href="http://dongbinhosting.dothome.co.kr/team/" target="_blank" class="site">사이트 보기</a>
-					</article>
-					<article class="port__item p4">
-						<span class="num">04.</span>
-						<a href="http://jok874.dothome.co.kr/main/" target="_blank" class="img">
-							<img src={port04} alt="TeamACE 포트폴리오" />
-						</a>
-						<h3 class="title">Team Ace 팀프로젝트</h3>
-						<p class="desc">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam
-							facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam
-							tenetur in dolores officia voluptatum?
-						</p>
-						<a href="http://jok874.dothome.co.kr/main/" target="_blank" class="site">사이트 보기</a>
-					</article>
-					<article class="port__item p5">
-						<span class="num">05.</span>
-						<a href="http://kimmina.dothome.co.kr/hwasung/" class="img" target="_blank">
-							<img src={port05} alt="화성 포트폴리오" />
-						</a>
-						<h3 class="title">화성 포트폴리오</h3>
-						<p class="desc">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam
-							facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam
-							tenetur in dolores officia voluptatum?
-						</p>
-						<a href="http://kimmina.dothome.co.kr/hwasung/" target="_blank" class="site">사이트 보기</a>
-					</article>
-					<article class="port__item p6">
-						<span class="num">06.</span>
-						<a href="http://dongbinhosting.dothome.co.kr/team/" class="img" target="_blank">
-							<img src={port06} alt="여다슬 포트폴리오" />
-						</a>
-						<h3 class="title">Team_C 포트폴리오</h3>
-						<p class="desc">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, nam illum? Numquam aperiam
-							facere quod corporis hic consequuntur, repellat cum saepe, neque dolorum cumque veniam
-							tenetur in dolores officia voluptatum?
-						</p>
-						<a href="http://dongbinhosting.dothome.co.kr/team/" target="_blank" class="site">사이트 보기</a>
-					</article>
+					{portTExt.map((port, key) => (
+						<article class={`port__item p${key + 1}`} key={key}>
+							<span class="num">{port.num}</span>
+							<a href={port.site} target="_blank" class="img">
+								<img src={port.img} alt={port.title} />
+							</a>
+							<h3 class="title">{port.title}</h3>
+							<p class="desc">{port.desc}</p>
+							<a href={port.site} target="_blank" class="site">사이트 보기</a>
+						</article>
+					))}
+
+
 				</div>
 			</div>
 		</section>
